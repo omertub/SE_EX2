17e8dbe714dc01439c196016da0de30a4442b9fc
Omer Tubul <omerishn@gmail.com>
2021-04-19 09:56:26 +0300

    sms manager

--- se_ex2/SmsManager.java
+++ se_ex2/SmsManager.java
@@ -1,6 +1,49 @@
 package se_ex2;
 
-public class SmsManager {
-    //ArrayList<SmsConversation> 
-    //public add
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class SmsManager implements AppManager{
+    HashMap<Contact, LinkedList<String>> conversations;
+
+
+    public SmsManager() {
+        conversations = new HashMap<Contact, LinkedList<String>>();
+    }
+
+    public void addEntry(Contact c, String s) {
+        LinkedList<String> conversation = conversations.get(c);
+        if (conversation == null) {
+            conversation = new LinkedList<String>();
+            conversations.put(c, conversation);
+        }
+        conversation.add(s);
+    }
+
+    @Override
+    public void removeEntry(Contact c) {
+        conversations.remove(c);
+    }
+    
+    public void printConversation (Contact c) {
+        System.out.println(conversations.get(c));
+    }
+    
+    public void findSentence(String s) {
+        for (HashMap.Entry<Contact, LinkedList<String>> entry : conversations.entrySet()) {
+            LinkedList<String> conversation = entry.getValue();
+            Iterator<String> iter = conversation.iterator();
+            while (iter.hasNext()) {
+                String str = iter.next();
+                if (str.contains(s))
+                    System.out.println(entry.getKey());
+            }
+        }
+    }
+    
+    public void printAll() {
+        for (HashMap.Entry<Contact, LinkedList<String>> entry : conversations.entrySet())
+            printConversation(entry.getKey());
+    }
 }
